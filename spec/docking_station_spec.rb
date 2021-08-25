de76990b733139abce1d2e66ff66187e326cfb88
LauraMaddaluno <lauralondon.4@gmail.com>
2021-08-25 16:19:02 +0000

    add instance variable and attr_reader and passed test for the bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,11 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
   it { is_expected.to respond_to(:dock).with(1).argument }
   it { is_expected.to respond_to :bike }
+  it "return bike" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike 
+  end
 
   it 'docks bike' do
     bike = Bike.new
