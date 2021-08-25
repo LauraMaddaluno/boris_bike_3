f2482aee7215b2cfec44c7f0f9a9ba1c83dcfa79
Tom Lee <tomadamlee02@gmail.com>
2021-08-25 16:56:33 +0100

    added docking method, along with relevant spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,13 @@ require "docking_station"
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to :bike }
+
+  it 'docks bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
 
   it 'releases working bikes' do
     bike = subject.release_bike
