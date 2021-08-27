3d8a88fd3d8522b174aff9f9f6f4f5e29e5a3520
LauraMaddaluno <lauralondon.4@gmail.com>
2021-08-27 16:12:57 +0000

    exerc 14 Add test for increased capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,18 +3,18 @@ require "docking_station"
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
   it { is_expected.to respond_to(:dock).with(1).argument }
-  it { is_expected.to respond_to :bike }
+  it { is_expected.to respond_to :bikes }
    
   
   it "return bike" do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike 
+    expect(subject.bikes).to eq [bike] 
   end
 
   it 'docks bike' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq [bike]
   end
 
   it 'releases working bikes' do
@@ -27,8 +27,9 @@ describe DockingStation do
   end
 
   it 'raise an error when docking_station is full' do
-    subject.dock(Bike.new)
+    20.times { subject.dock(Bike.new) }
     expect{ subject.dock(Bike.new) }.to raise_error "Docking station is full"
   end
+  
   
 end
