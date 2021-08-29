25c91ac6b31997277e4807b452274b767cea94da
LauraMaddaluno <lauralondon.4@gmail.com>
2021-08-29 23:18:36 +0000

    ch 17 initialization default capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,31 +1,36 @@
 require_relative 'bike'
 
 class DockingStation
+  
   DEFAULT_CAPACITY = 20
   attr_reader :bikes
+  attr_reader :capacity
+  attr_accessor :capacity
   
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @bikes = []
+    @capacity = capacity
   end
   
   def release_bike
     fail "No bikes available" if empty?
-    @bikes.pop
+    bikes.pop
   end
 
   def dock(bike)
     fail "Docking station is full" if full?
-    @bikes << bike
+    bikes << bike
   end
 
   private
+  #attr_reader :bikes
 
   def full?
-    @bikes.count >= DEFAULT_CAPACITY
+    bikes.count >= capacity
   end
 
   def empty?
-    @bikes.empty?
+    bikes.empty?
   end
   
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,7 @@ require "docking_station"
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
   it { is_expected.to respond_to(:dock).with(1).argument }
-  it { is_expected.to respond_to :bikes }
+  it { is_expected.to respond_to(:bikes) }
    
   
   it "return bike" do
@@ -25,9 +25,25 @@ describe DockingStation do
   it 'raise an error when docking_station is empty'do 
     expect{subject.release_bike}.to raise_error "No bikes available"
   end
-
+  
   it 'raise an error when docking_station is full' do
-    DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
+    subject.capacity.times { subject.dock(Bike.new) }
     expect{ subject.dock(Bike.new) }.to raise_error "Docking station is full"
   end
+  
+  
+  it 'has a default capacity of 20'do
+  expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+  
+ 
+  
+  let(:bike) { Bike.new }
+  it 'defaults capacity' do
+  DockingStation::DEFAULT_CAPACITY.times do
+      subject.dock(bike)
+    end
+    expect{ subject.dock(Bike.new) }.to raise_error "Docking station is full"
+  end
+
 end
